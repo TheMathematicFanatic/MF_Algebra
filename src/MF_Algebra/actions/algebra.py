7189f59643e6938a254da801f0f8fe6ba753aa53
John Connell <johnconnelltutor@gmail.com>
2025-07-23 17:27:11 -0400

    Reverted that method hotswapping idea, that was insane lol, in favor of static addressmap swapping which should be good for algebraic actions

--- src/MF_Algebra/actions/algebra.py
+++ src/MF_Algebra/actions/algebra.py
@@ -1,6 +1,6 @@
 from .action_core import *
 from ..expressions.expression_common import *
-from MF_Tools.dual_compatibility import PI, FadeIn, FadeOut
+from MF_Tools.dual_compatibility import PI, FadeIn, FadeOut, Write, Uncreate
 from .action_common import swap_children_
 
 
@@ -11,6 +11,7 @@ class AlgebraicAction(Action):
         self.template2 = template2
         self.var_kwarg_dict = var_kwarg_dict #{a:{"path_arc":PI}}
         self.extra_addressmaps = extra_addressmaps
+        self.addressmap = None
     
     @preaddressfunc
     def get_output_expression(self, input_expression=None):
@@ -20,6 +21,10 @@ class AlgebraicAction(Action):
     @preaddressmap
     def get_addressmap(self, input_expression=None):
         # Best overwritten in subclasses, but this gets the job done sometimes.
+        # Actually, I think most subclasses will have a static addressmap, so I'll add this line at the start.
+        if self.addressmap is not None:
+            return self.addressmap
+
         addressmap = []
         def get_var_ad_dict(template):
             template_leaves = {
@@ -61,48 +66,35 @@ class EquationManeuver(AlgebraicAction):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     
-
     def reverse(self):
         # swaps input and output templates
         self.template1, self.template2 = self.template2, self.template1
-        # intercepts and modifies addressmap accordingly
+        # modifies addressmap accordingly
         # (swap order and negate path_arcs)
-        old_addressmap_method = self.get_addressmap
-        def new_addressmap_method(self, input_expression=None):
-            addressmap = old_addressmap_method(input_expression)
-            for entry in addressmap:
-                entry[0], entry[1] = entry[1], entry[0]
-                if len(entry) == 3 and 'path_arc' in entry[2].keys():
-                    entry[2]['path_arc'] = -entry[2]['path_arc']
-            return addressmap
-        self.get_addressmap = new_addressmap_method
+        for entry in self.addressmap:
+            entry[0], entry[1] = entry[1], entry[0]
+            if len(entry) == 3 and 'path_arc' in entry[2].keys():
+                entry[2]['path_arc'] = -entry[2]['path_arc']
         return self
-    
 
     def flip(self):
         # flips the equations of both templates
         s = swap_children_()
         self.template1 = s.get_output_expression(self.template1)
         self.template2 = s.get_output_expression(self.template2)
-        # intercepts and modifies addressmap accordingly
+        # modifies addressmap accordingly
         # (swap first character of addresses and negate path_arcs)
-        old_addressmap_method = self.get_addressmap
-        def new_addressmap_method(self, input_expression=None):
-            addressmap = old_addressmap_method(input_expression)
-            for entry in addressmap:
-                if isinstance(entry[0], str):
-                    entry[0] = str(1-int(entry[0][0])) + entry[0][1:]
-                if isinstance(entry[1], str):
-                    entry[1] = str(1-int(entry[1][0])) + entry[1][1:]
-                if len(entry) == 3 and 'path_arc' in entry[2].keys():
-                    entry[2]['path_arc'] = -entry[2]['path_arc']
-            return addressmap
-        self.get_addressmap = new_addressmap_method
+        for entry in self.addressmap:
+            if isinstance(entry[0], str):
+                entry[0] = str(1-int(entry[0][0])) + entry[0][1:]
+            if isinstance(entry[1], str):
+                entry[1] = str(1-int(entry[1][0])) + entry[1][1:]
+            if len(entry) == 3 and 'path_arc' in entry[2].keys():
+                entry[2]['path_arc'] = -entry[2]['path_arc']
         return self
 
-
     def reverse_flip(self):
-        return self.reverse().flip() 
+        return self.reverse().flip()
     
     
 
@@ -118,9 +110,7 @@ class alg_add_R(EquationManeuver):
             a & c - b,
             **kwargs
         )
-
-    def get_addressmap(self, input_expression=None):
-        return (
+        self.addressmap = (
             ['01', '11', {'path_arc':PI}],
             ['0+', '1-', {'path_arc':PI}],
         )
@@ -133,9 +123,7 @@ class alg_add_L(EquationManeuver):
             b & c - a,
             **kwargs
         )
-    
-    def get_addressmap(self, input_expression=None):
-        return (
+        self.addressmap = (
             ['00', '11', {'path_arc':PI}],
             ['0+', '1-', {'path_arc':PI}]
         )
@@ -144,3 +132,71 @@ class alg_add_L(EquationManeuver):
         #     ['0+', FadeOut, {'run_time':0.5}],
         #     [FadeIn, '1-', {'run_time':0.5, 'delay':0.5}]
         # )
+
+
+class alg_mul_R(EquationManeuver):
+    def __init__(self, **kwargs):
+        super().__init__(
+            a * b & c,
+            a & c / b,
+            **kwargs
+        )
+        self.addressmap = (
+            ['01', '11', {'path_arc':PI}],
+            #['0*', FadeOut, {'run_time':0.5}],
+            [Write, '1/', {'run_time':0.5, 'delay':0.5}]
+        )
+    
+    def reverse(self):
+        super().reverse()
+        self.addressmap = (
+            ['11', '01', {'path_arc':-PI}],
+            ['1/', FadeOut, {'run_time':0.5, 'delay':0}]
+        )
+        return self
+
+
+class alg_mul_L(EquationManeuver):
+    def __init__(self, **kwargs):
+        super().__init__(
+            a * b & c,
+            b & c / a,
+            **kwargs
+        )
+        self.addressmap = (
+            ['00', '11', {'path_arc':PI}],
+            #['0*', FadeOut, {'run_time':0.5}],
+            [Write, '1/', {'run_time':0.5, 'delay':0.5}]
+        )
+
+    def reverse(self):
+        super().reverse()
+        self.addressmap = (
+            ['11', '00', {'path_arc':-PI}],
+            ['1/', FadeOut, {'run_time':0.5, 'delay':0}]
+        )
+        return self
+
+
+# class alg_pow_R(EquationManeuver):
+#     def __init__(self, **kwargs):
+#         super().__init__(
+#             a**b & c,
+#             a & rad(b)(c),
+#             **kwargs
+#         )
+
+
+
+
+
+
+
+
+class difference_of_squares_(AlgebraicAction):
+    def __init__(self, **kwargs):
+        super().__init__(
+            a**2 - b**2,
+            (a+b)*(a-b),
+            **kwargs
+        )
