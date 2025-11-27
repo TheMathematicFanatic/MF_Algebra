6e97277c1ffc15c0eb1da2a36397fd6b025270f4
John Connell <johnconnelltutor@gmail.com>
2025-11-26 22:37:38 -0500

    Finally found a simple idiom to reduce this!!

--- src/MF_Algebra/actions/apply_operation.py
+++ src/MF_Algebra/actions/apply_operation.py
@@ -7,10 +7,12 @@ from MF_Tools.dual_compatibility import Write
 
 
 class apply_operation_(Action):
-	OpClass = None
-	def __init__(self, other, side='right', introducer=Write, **kwargs):
+	OpClass = NotImplemented
+	side = 'right'
+	def __init__(self, other, OpClass=None, side=None, introducer=Write, **kwargs):
 		self.other = Smarten(other)
-		self.side = side
+		self.OpClass = OpClass or self.OpClass
+		self.side = side or self.side
 		self.introducer = introducer
 		super().__init__(**kwargs)
 
@@ -43,7 +45,7 @@ class apply_operation_(Action):
 			raise ValueError(f'Invalid side: {self.side}. Must be left or right.')
 
 	def __repr__(self):
-		return type(self).__name__ + '(\'' + str(self.preaddress) + '\', ' + str(self.other) + ')'
+		return type(self).__name__ + '(' + str(self.other) + ')'
 
 
 class add_(apply_operation_):
@@ -66,5 +68,4 @@ class equals_(apply_operation_):
 
 class apply_func_(apply_operation_):
 	OpClass = ApplyFunction
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, side='left', **kwargs)
+	side = 'right'
