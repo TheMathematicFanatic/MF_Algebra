24571e4b2ddfc9e83b14dc72804878ab9b5555eb
John Connell <johnconnelltutor@gmail.com>
2026-04-14 23:14:11 -0400

    Partially finished rewrap rewrite

--- src/MF_Algebra/algebra/wrap.py
+++ src/MF_Algebra/algebra/wrap.py
@@ -1,9 +1,11 @@
 from .algebra_core import AlgebraicAction
 from ..expressions import Expression
 
-
+#TODO I did not finish what I was doing here but I gotta commit changes and I gotta go to bed
 
 class rewrap_subex_(AlgebraicAction):
+	auto_morph = True
+	auto_resolve_kwargs = {'path_arc':1, 'lag_ratio':0.03, 'delay':0.25}
 	def __init__(self,
 		start_exp:Expression = None,
 		end_exp:Expression = None,
@@ -13,48 +15,39 @@ class rewrap_subex_(AlgebraicAction):
 		self.target_subex = target_subex
 		self.start_exp = start_exp
 		self.end_exp = end_exp
-		start_ads = start_exp.get_addresses_of_subex(target_subex)
-		end_ads = end_exp.get_addresses_of_subex(target_subex)
-		assert len(start_ads) == len(end_ads) == 1, 'Target subexpression must be unique in both expressions.'
-		# I'd love to make it work for several but I don't know how to get all glyphs except for more than one subexpression in an addressmap
-		self.start_ad = start_ads[0]
-		self.end_ad = end_ads[0]
+		self.start_ads = start_exp.get_addresses_of_subex(target_subex)
+		self.end_ads = end_exp.get_addresses_of_subex(target_subex)
 		super().__init__(self.start_exp, self.end_exp, **kwargs)
 	
 	def get_addressmap(self, input_expression=None):
-		return [
-			[self.start_ad, self.end_ad, {'delay':0.25}],
-			['!'+self.start_ad, '!'+self.end_ad, {'path_arc':3}],
-		]
+		if len(self.start_ads) == len(self.end_ads):
+			return [
+				[start_ad, end_ad]
+				for start_ad, end_ad in zip(self.start_ads, self.end_ads)
+			]
+		else:
+			return [
+				[start_ad, end_ad]
+				for start_ad in self.start_ads for end_ad in self.end_ads
+			]
 
 
-class wrap_subex_(rewrap_subex_):
+
+
+class unwrap_subex_(rewrap_subex_):
 	def __init__(self,
 		outer_exp,
+		target_subex,
 		**kwargs
 	):
 		super().__init__(
 			start_exp = outer_exp,
-			end_exp = None,
-			target_subex = None,
+			end_exp = target_subex,
+			target_subex = target_subex,
 			**kwargs
 		)
-	
 
 
-
-# class unwrap_subex_(rewrap_subex_):
-# 	def __init__(self,
-# 		outer_exp,
-# 		target_subex,
-# 		**kwargs
-# 	):
-# 		super().__init__(
-# 			start_exp = outer_exp,
-# 			end_exp = target_subex,
-# 			target_subex = target_subex,
-# 			**kwargs
-# 		)
 
 from ..actions import Action
 class unwrap_subex_(Action):
